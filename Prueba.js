9ac848290866d88e79d108fde9396dba67b8c953
ZeroTF80 <153334562+ZeroTF80@users.noreply.github.com>
2023-12-08 21:41:24 -0300

    Add files via upload

---
+++ Prueba.js
@@ -0,0 +1,13 @@
+// Base de datos.
+var baseDeDatos = {
+  tierra: "Suelo común",
+  planta: "Nombre de una planta"
+};
+
+// Imprimir texto en base a una base de datos, mas un texto.
+console.log("Descripción de la tierra:", baseDeDatos.tierra);
+
+// Inventario inicial
+let inventario = {
+  madera: 0
+};
